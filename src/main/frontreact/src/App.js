b48e83e5faa7c96fc36879407fcd4cc9f5bc6a78
chaejunho689 <smile689@naver.com>
2024-11-15 14:49:50 +0900

    2024115

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spring Boot + React + NCP </h1>
+      <h3> 카운트 : 5 </h3> 
+      <img src="./image/1.jpg" alt="옴팡이" />
+    </div>
+  );
+}
+
+export default App;
